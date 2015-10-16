3cc043a07c07296db694ac3b4426df037c638b56
Jade Loyzaga <jadel@cse.unsw.edu.au>
2015-10-15 22:00:13 -0700

    Checking for valid moves in the 3x3 grid
    Implemented maps with Arrays, since simply accessing the key is just an index and the data isn't sparse.
    
    This mapping could even be passed from a server, if the are other sudoku types that need to be supported. Eg, Hexidecimal, samuri, or 6x6.
    
    TODO:
    - If a change to a cell no longer clashes with another cell, check that cell for any new clashes that have been created. At the moment a false positive can be achieved.
    - Clear button and reset button functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,45 @@
 var startGame = [
-                "5","3","",     "","7","",      "","","",
-                "6","","",      "1","9","5",    "","","",
-                "","9","8",     "","","",       "","6","",  
+                  "5","3","",     "","7","",      "","","",
+                  "6","","",      "1","9","5",    "","","",
+                  "","9","8",     "","","",       "","6","",  
 
-                "8","","",      "","6","",      "","","3",  
-                "4","","",      "8","","3",     "","","1",  
-                "7","","",      "","2","",      "","","6",  
+                  "8","","",      "","6","",      "","","3",  
+                  "4","","",      "8","","3",     "","","1",  
+                  "7","","",      "","2","",      "","","6",  
                 
-                "","6","",      "","","",       "2","8","",
-                "","","",       "4","1","9",    "","","5",
-                "","","",       "","8","",      "","7","9"
-                ]
+                  "","6","",      "","","",       "2","8","",
+                  "","","",       "4","1","9",    "","","5",
+                  "","","",       "","8","",      "","7","9"
+                ];
 
+var idToGridMap = [ 0, 0, 0,  1, 1, 1,  2, 2, 2, 
+                    0, 0, 0,  1, 1, 1,  2, 2, 2, 
+                    0, 0, 0,  1, 1, 1,  2, 2, 2, 
+
+                    3, 3, 3,  4, 4, 4,  5, 5, 5, 
+                    3, 3, 3,  4, 4, 4,  5, 5, 5, 
+                    3, 3, 3,  4, 4, 4,  5, 5, 5, 
+
+                    6, 6, 6,  7, 7, 7,  8, 8, 8,                     
+                    6, 6, 6,  7, 7, 7,  8, 8, 8,                     
+                    6, 6, 6,  7, 7, 7,  8, 8, 8 
+                  ];
+                  
+
+var gridToIdMap = [ [0,1,2,9,10,11,18,19,20],       // grid 0
+                    [3,4,5,12,13,14,21,22,23],      // grid 1
+                    [6,7,8,15,16,17,24,25,26],      // grid 2
+                    [27,28,29,36,37,38,45,46,47],   // grid 3
+                    [30,31,32,39,40,41,48,49,50],   // grid 4
+                    [33,34,35,42,43,44,51,52,53],   // grid 5
+                    [54,55,56,63,64,65,72,73,74],   // grid 6
+                    [57,58,59,66,67,68,75,76,77],   // grid 7
+                    [60,61,62,69,70,71,78,79,80]    // grid 8
+                  ];
 var board = [];
 var inputValue = '';
 
 $(document).ready(function() {
-
-
     $(".valueButton").click(function() {
         inputValue = $(this).text();
         
@@ -27,10 +49,14 @@ $(document).ready(function() {
         $(this).attr("ID", "value");
     });
 
+    $(".delButton").click(function() {
+        inputValue = '';
+    });
+
     $(".editable").click(function() {
-        if (inputValue == '') {
-            console.log("select a number to add to this cell");
-        } else {
+        // if (inputValue == '') {
+        //     console.log("select a number to add to this cell");
+        // } else {
 
 
             board[$(this).attr("id")] = inputValue;
@@ -38,7 +64,7 @@ $(document).ready(function() {
             console.log(board);
             isValid($(this));
             // alert("row is " + allInRow(this));
-        }
+        // }
     });
 });
 
@@ -83,8 +109,7 @@ function getCol(y) {
 
 function isValid(cell) {
     var cellId = $(cell).attr("id");
-    var valid = checkRow(cellId) && checkCol(cellId);
-
+    var valid = checkGrid(cellId) && checkRow(cellId) && checkCol(cellId);
 
     if (!valid) {
         $(cell).addClass("invalid");
@@ -108,13 +133,13 @@ function checkRow(cellId) {
         if (getCol(cellId) == i) {
             continue;
         } else if (cellVal == checkVal) {
-            console.log("we have a match " + cellVal + " matches " + (checkIndex) + ":" + $(board)[checkIndex]);
+            // console.log("we have a match " + cellVal + " matches " + (checkIndex) + ":" + $(board)[checkIndex]);
             valid = false;
             break;
             highlightClash(checkVal);
         } else {
             // remove invalid class if it's there
-            $("#"+checkIndex).removeClass("invalid");
+            // $("#"+checkIndex).removeClass("invalid");
         }
     }
     return valid;
@@ -134,14 +159,43 @@ function checkCol(cellId) {
         if (getRow(cellId) == i) {
             continue;
         } else if (cellVal == checkVal) {
-            console.log("we have a match " + cellVal + " matches " + checkIndex + ":" + $(board)[checkIndex]);
+            // console.log("we have a match " + cellVal + " matches " + checkIndex + ":" + $(board)[checkIndex]);
             valid = false;
             break;
             highlightClash(checkVal);
         } else {
             // remove invalid class if it's there
-            $("#"+checkIndex).removeClass("invalid");
+            // $("#"+checkIndex).removeClass("invalid");
+        }
+    }
+    return valid;
+}
 
+function checkGrid(cellId) {
+    var valid = true;
+    var cellVal = $(board)[cellId];
+    
+    //work out which grid the cell lives in
+    var grid = idToGridMap[cellId];
+    // find the other ids that live in that grid
+    var gridIds = gridToIdMap[grid];
+
+    console.log("looking for ids " + gridIds);
+
+    for (i = 0; i < 9; i++) {
+        checkIndex = gridIds[i];
+        var checkVal = board[checkIndex];
+
+        if (cellId == checkIndex) {
+            continue;
+        } else if (cellVal == checkVal) {
+            // console.log("we have a match " + cellVal + " matches " + checkIndex + ":" + $(board)[checkIndex]);
+            valid = false;
+            break;
+            highlightClash(checkVal);
+        } else {
+            // remove invalid class if it's there
+            // $("#"+checkIndex).removeClass("invalid");
         }
     }
     return valid;
